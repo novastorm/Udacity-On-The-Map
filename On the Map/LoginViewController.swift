d9db0b1a30bb49b82edd1b495618ce7a60104d65
Adland Lee <adland@4mfd.com>
2016-03-24 16:07:59 -0700

    Add Login View design

---
+++ On the Map/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.swift
+//  On the Map
+//
+//  Created by Adland Lee on 3/24/16.
+//  Copyright © 2016 Adland Lee. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func login(sender: AnyObject) {
+        print("login")
+        print("\(emailField.text): \(passwordField.text)")
+    }
+    
+    @IBAction func signUp(sender: AnyObject) {
+        print("signup")
+    }
+}
+
