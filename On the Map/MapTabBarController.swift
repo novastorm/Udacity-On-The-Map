acbb15ab7777cdf0ab3d10b1d4a5313025ff74ab
Adland Lee <adland@4mfd.com>
2016-03-26 14:57:10 -0700

    add map views

---
+++ On the Map/MapTabBarController.swift
@@ -0,0 +1,24 @@
+//
+//  MapTabBarController.swift
+//  On the Map
+//
+//  Created by Adland Lee on 3/26/16.
+//  Copyright © 2016 Adland Lee. All rights reserved.
+//
+
+import UIKit
+
+class MapTabBarController: UITabBarController {
+    
+    @IBAction func logout(sender: AnyObject) {
+        print("logout")
+    }
+    
+    @IBAction func refresh(sender: AnyObject) {
+        print("refresh")
+    }
+    
+    @IBAction func showInformationDetail(sender: AnyObject) {
+        print("showInformationDetail")
+    }
+}
