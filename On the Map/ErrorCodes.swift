9d35acdbbeca2ea9f8f5542411d39f43c4b6c599
Adland Lee <adland@4mfd.com>
2016-03-30 20:43:34 -0700

    add files

---
+++ On the Map/ErrorCodes.swift
@@ -0,0 +1,9 @@
+//
+//  ErrorCodes.swift
+//  On the Map
+//
+//  Created by Adland Lee on 3/30/16.
+//  Copyright © 2016 Adland Lee. All rights reserved.
+//
+
+import Foundation
---
+++ On the Map/Utilities.swift
@@ -0,0 +1,9 @@
+//
+//  Utilities.swift
+//  On the Map
+//
+//  Created by Adland Lee on 3/29/16.
+//  Copyright © 2016 Adland Lee. All rights reserved.
+//
+
+import Foundation
