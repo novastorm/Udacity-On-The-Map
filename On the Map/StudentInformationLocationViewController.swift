61ae68a7a44f9452ef3cf3849e8a9b7f777909aa
Adland Lee <adland@4mfd.com>
2016-04-07 15:39:20 -0700

    Add GeoLocation processing

--- On the Map/StudentInformationLocationViewController.swift
+++ On the Map/StudentInformationLocationViewController.swift
@@ -6,12 +6,13 @@
 //  Copyright © 2016 Adland Lee. All rights reserved.
 //
 
+import CoreLocation
 import Foundation
 import UIKit
 
 class StudentInformationLocationViewController: UIViewController {
     
-    var account: Account!
+    var geocoder: CLGeocoder?
     
     @IBOutlet weak var locationTextField: UITextField!
     
@@ -23,8 +24,44 @@ class StudentInformationLocationViewController: UIViewController {
         print("findOnTheMap")
         print("location \(locationTextField.text)")
 
+        guard let addressString = locationTextField.text else {
+            showAlert(self, title: nil, message: "Unable to unwrap location string")
+            return
+        }
+        
+        if geocoder == nil {
+            geocoder = CLGeocoder()
+        }
+
+        guard let geocoder = geocoder else {
+            showAlert(self, title: "Error", message: "Error unwrapping geocoder object")
+            return
+        }
+        
+        geocoder.cancelGeocode()
+        geocoder.geocodeAddressString(addressString, completionHandler: { (placemarks, error) in
+            if error != nil {
+                showAlert(self, title: "Error", message: "\(error)")
+                return
+            }
+            
+            guard let placemark = placemarks?.first else {
+                showAlert(self, title: nil, message: "Unable to get first plackmark")
+                return
+            }
+            
+            self.showStudentInformationURLView(placemark)
+        })
+    }
+    
+    func showStudentInformationURLView(placemark: CLPlacemark) {
         guard let storyboard = storyboard else {
             print("Unable to get storyboard")
+            return
+        }
+        
+        guard let presentingVC = self.presentingViewController else {
+            print("Unable to get presenting view controller")
             return
         }
         
@@ -33,11 +70,8 @@ class StudentInformationLocationViewController: UIViewController {
             return
         }
         
-        guard let presentingVC = self.presentingViewController else {
-            print("Unable to get presenting view controller")
-            return
-        }
-
+        destinationVC.placemark = placemark
+        
         dismissViewControllerAnimated(false) {
             presentingVC.presentViewController(destinationVC, animated: true) {}
         }
--- On the Map/StudentInformationURLViewController.swift
+++ On the Map/StudentInformationURLViewController.swift
@@ -6,14 +6,32 @@
 //  Copyright © 2016 Adland Lee. All rights reserved.
 //
 
+import CoreLocation
 import Foundation
+import MapKit
 import UIKit
 
 class StudentInformationURLViewController: UIViewController {
     
-    var account: Account!
-    
     @IBOutlet weak var URLTextField: UITextField!
+    @IBOutlet weak var mapView: MKMapView!
+
+    var placemark: CLPlacemark!
+    
+    var account: Account? {
+        return UdacityClient.sharedInstance().account
+    }
+    
+    override func viewDidLoad() {
+//        print(placemark)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = (placemark.location?.coordinate)!
+        mapView.addAnnotation(annotation)
+
+        let region = MKCoordinateRegionMakeWithDistance(annotation.coordinate, 500, 500)
+        mapView.setRegion(region, animated: true)
+    }
     
     @IBAction func cancel(sender: AnyObject) {
         print("StudentInformationURLViewController::cancel")
